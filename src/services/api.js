6d057d6cecff890e6931554b77d20cd020780eca
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2025-09-01 17:02:34 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080' // Mudar para url da api
+    // baseURL: 'http://localhost:8080' // Mudar para url da api
+    baseURL: `https://decent-gerladina-lucassan1-2434bea0.koyeb.app/`
 });
