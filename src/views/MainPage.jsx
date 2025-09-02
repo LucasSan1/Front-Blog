2f43ba8981640c68f5ba5f98aec345d81bd53fd8
LucasSan1 <evangelistalucas034@gmail.com>
2025-09-01 23:22:05 -0300

    update - botao novo post atualizado

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import PostBox from "../components/PostBox";
 import NovoPost from "../components/ModalPost";
+import { FiPlus } from "react-icons/fi";
 
 const MainPage = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,12 +16,11 @@ const MainPage = () => {
                 <PostBox searchTerm={searchTerm} />
             </div>
 
-            <button
-                className={`fixed bottom-4 right-4 bg-[#FF6F61] text-white text-2xl p-4 rounded-full shadow-lg transition-transform duration-200
-                ${isModalOpen ? "scale-75" : "scale-100"}`}
+           <button
+                className="fixed bottom-4 right-4 bg-[#FF6F61] text-white text-xl p-3 rounded-full shadow-lg hover:scale-105 transition-transform duration-200"
                 onClick={() => setIsModalOpen(true)}
             >
-                Novo post
+                <FiPlus />
             </button>
 
             <NovoPost 
