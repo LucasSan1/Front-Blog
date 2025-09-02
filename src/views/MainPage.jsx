856f4f6537352bc8f1dee624a393f5781e90968f
LucasSan1 <evangelistalucas034@gmail.com>
2025-09-01 23:18:44 -0300

    Fix - botao novo post

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -15,8 +15,9 @@ const MainPage = () => {
                 <PostBox searchTerm={searchTerm} />
             </div>
 
-            <button 
-                className="fixed bottom-4 right-4 bg-[#FF6F61] text-white text-2xl p-4 rounded-full shadow-lg hover:scale-105 transition-transform duration-200"
+            <button
+                className={`fixed bottom-4 right-4 bg-[#FF6F61] text-white text-2xl p-4 rounded-full shadow-lg transition-transform duration-200
+                ${isModalOpen ? "scale-75" : "scale-100"}`}
                 onClick={() => setIsModalOpen(true)}
             >
                 Novo post
